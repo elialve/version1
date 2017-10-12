bfc3585b763f264065e012299da0a5cf7ca62d47
elialve <eliasalveal18@gmail.com>
2017-10-12 12:03:31 -0300

    puerto cambio

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 app.set('view engine', 'jade');
 app.use(express.static('public'));
 
-
+const port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   res.render('index', { title: ' Pagina', message: 'Bienvenidos'});
 });
@@ -11,6 +11,6 @@ app.get('/paralax', function (req, res) {
   res.render('paralax', { title: ' Pagina', message: 'Bienvenidos'});
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ${port}!');
 });
