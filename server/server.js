f61c4e450decdd1b417252d0b8a3ca467f437cfa
elialve <eliasalveal18@gmail.com>
2017-10-13 20:28:31 -0300

    Agregar buscar/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Producto} = require('./models/producto.js');
@@ -9,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 //Agregar
-app.post('/productos', (req, res) => {
+app.post('/prodAdd', (req, res) => {
   var prod = new Producto({
     nombre: req.body.nombre,
     precio: req.body.precio,
@@ -24,14 +25,29 @@ app.post('/productos', (req, res) => {
 });
 
 //Listar
-app.get('/productos', (req, res) => {
+app.get('/prodList', (req, res) => {
     Producto.find().then((prod) => {
       res.send({prod});
     }, (e) => {
       res.status(400).send(e);
     });
 })
+//Buscar
+app.get('/prodBuscar/:id', (req, res) => {
+    var id = req.params.id;
 
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+    Producto.findById(id).then((prod) => {
+      if (!prod) {
+        return res.status(404).send();
+      }
+      res.send({prod});
+    }).catch((e) => {
+      res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
   console.log('Inicio puerto ', port);
