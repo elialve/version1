fc624f59a60da3d9125feaf0ef994f405e2b6c12
elialve <eliasalveal18@gmail.com>
2017-10-15 15:30:03 -0300

    Agregue vista listar

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -5,13 +6,36 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Producto} = require('./models/producto.js');
 
+var session = require('express-session');
+var Cart = require('./models/cart.js');
+var cookieParser = require('cookie-parser');
+
 var app = express();
+var router = express.Router();
 app.set('view engine', 'jade');
 app.use(express.static('public'));
 const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+
 app.get('/', function (req, res) {
-  res.render('index', { title: ' Pagina', message: 'Bienvenidos'});
+      Producto.find().then((prod) => {
+          res.render('index', { productos : prod});
+      }, (e) => {
+        res.status(400).send(e);
+      });
 });
 //Agregar
 app.post('/prodAdd', (req, res) => {
@@ -53,6 +77,22 @@ app.get('/prodBuscar/:id', (req, res) => {
       res.status(400).send();
     });
 });
+
+
+app.get('/add/:id', function(req, res) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  Producto.findById(productId).then((prod) => {
+    if (!prod) {
+      return res.status(404).send();
+    }
+    cart.add(prod, productId);
+    req.session.cart = cart;
+    res.redirect('/');
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 //Eliminar
 app.delete('/prodDel/:id',(req, res) =>{
   var id = req.params.id;
@@ -70,6 +110,32 @@ app.delete('/prodDel/:id',(req, res) =>{
   res.status(400).send();
   });
 });
+
+app.patch('/prodMod/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Producto.findByIdAndUpdate(id, {$set:body}, {new: true}).then((prod) =>{
+    if (!prod) {
+      return res.status(404).send();
+    }
+    res.send({prod});
+  }).catch((e) =>{
+    res.status(400).send();
+  });
+});
+
+
 app.listen(port, () => {
   console.log('Inicio puerto ', port);
 });
